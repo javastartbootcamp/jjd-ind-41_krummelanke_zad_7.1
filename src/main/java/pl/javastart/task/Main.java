a61429249213a9dd4f25c2bad917648d78b9194d
_245_pawel <lanke@wp.pl>
2023-04-03 23:00:12 +0200

    uprościłem sprawdzanie palindromu, zgodnie z uwagami

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -5,10 +5,11 @@ public class Main {
     public static void main(String[] args) {
         PalindromeService palindromeService = new PalindromeService();
 
-        int[] array = new int[]{0, 1, 2, 1, 0};
+        int[] array = new int[]{0, 1, 7, 7, 1, 0};
 
         boolean isPalindrome = palindromeService.isPalindrome(array);
 
         System.out.println(isPalindrome);
+        // System.out.println(array.length / 2);
     }
 }
--- src/main/java/pl/javastart/task/PalindromeService.java
+++ src/main/java/pl/javastart/task/PalindromeService.java
@@ -2,31 +2,22 @@ package pl.javastart.task;
 
 public class PalindromeService {
 
-    private int[] reverseArray(int[] array) {
-        int lenght = array.length;
-
-        int[] reversedArray = new int[lenght];
-
-        for (int i = 0; i < reversedArray.length; i++) {
-            int j = lenght - 1 - i;
-            reversedArray[i] = array[j];
+    private boolean compareNumbers(int[] array) {
+        int tempSum = 0;
+        for (int i = 0; i < (array.length / 2); i++) {
+            tempSum = tempSum + array[i] - array[array.length - 1 - i];
         }
-        return reversedArray;
-    }
-
-    private int compareNumbers(int[] array) {
-        String reversedString = createString(reverseArray(array));
-        String originalString = createString(array);
-        int reversedNumber = Integer.parseInt(reversedString);
-        int originalNumber = Integer.parseInt(originalString);
-        int subtractionDifference = originalNumber - reversedNumber;
-        return subtractionDifference;
+        if (tempSum == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // nie zmieniaj sygnatury tej metody. Jest ona testowana w PalindromeServiceTest
     public boolean isPalindrome(int[] array) {
         if (checkArray(array)) {
-            if (compareNumbers(array) == 0) {
+            if (compareNumbers(array)) {
                 return true;
             } else {
                 return false;
@@ -56,13 +47,5 @@ public class PalindromeService {
         } else {
             return true;
         }
-    }
-
-    private String createString(int[] table) {
-        String stringOfNumbers = "";
-        for (int i = 0; i < table.length; i++) {
-            stringOfNumbers = stringOfNumbers + table[i];
-        }
-        return stringOfNumbers;
     }
 }
